37ec8789a21cbd8cf8c77a30effa2f9d730fdff6
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-07-17 14:58:08 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-  
+      <Navigation></Navigation>
     </div>
   );
 }
